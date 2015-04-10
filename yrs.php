7e3af7a39a34e8114dbf88e4d17005484b9af6c2
Kevin Lewis <kevin@jellybabi.es>
2015-04-10 20:25:00 +0100

    YRS Page, Page header responsiveness

--- yrs.php
+++ yrs.php
@@ -5,29 +5,25 @@
 			<img src="/assets/img/yrs/hyperlocal-logo.png" alt="Hyperlocal Logo">
 		</div>
 		<p class="intro">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti atque cum eligendi incidunt ducimus reiciendis, quis! Aspernatur praesentium eaque ipsum minus mollitia, ipsam, soluta explicabo, facilis qui alias delectus eligendi.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti atque cum eligendi incidunt ducimus reiciendis, quis! Aspernatur praesentium eaque ipsum minus mollitia, ipsam, soluta explicabo, facilis qui alias delectus eligendi. <a href="/contact">Find out more by contacting us</a>.
 		</p>
 
 		<ul class="roles">
 			<li>
-				<h2>18 &amp; Under <small>Hacker</small></h2>
+				<h2>18 &amp; Under - Hacker</h2>
 				<img src="/assets/img/yrs/young.png">
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur vitae ipsam ipsum id.
 				</p>
 			</li>
 			<li>
-				<h2>Over 18 <small>Mentor</small></h2>
+				<h2>Over 18 - Mentor</h2>
 				<img src="/assets/img/yrs/mentor.png">
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur vitae ipsam ipsum id.
 				</p>
 			</li>
 		</ul>
-
-		<div class="details">
-			<h2><a href="http://hl.yrs.io/centres/25">Find out more here</a></h2>
-		</div>
 	</div>
 
 
