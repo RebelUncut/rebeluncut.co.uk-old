2b47b467b3500a4e0ba27888cad5f337af3a63c0
NatHigginbottom <nat@rebeluncut.co.uk>
2015-04-22 21:42:47 +0100

    Added films Page

---
+++ films.php
@@ -0,0 +1,80 @@
+<?php 
+	$title = "From Steak to Plate with ASDA";
+	$keywords = "key1";
+	include "partials/_header.php";
+?>
+
+	<div class="case-study">
+
+		<div class="page-title">
+			<div class="wrapper">
+				<h1>From Steak to Plate with ASDA</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, nisi, nesciunt earum harum vitae nulla id perferendis culpa ab. 
+				</p>
+			</div>
+		</div>
+
+		<section>
+			<div class="copy match">
+				<div class="inner">
+					<h2>Conception</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptas quisquam adipisci odit officia, ratione porro recusandae temporibus cum dolor blanditiis distinctio veniam est voluptate, repellendus commodi, facere itaque velit.
+					</p>
+				</div>
+			</div>
+			<div class="img match" style="background-image: url(/assets/img/case-studies/asda/1.png);"></div>
+		</section>
+
+		<section>
+			<div class="img match" style="background-image: url(/assets/img/case-studies/asda/2.png);"></div>
+			<div class="copy match">
+				<div class="inner">
+					<h2>Ideation</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptas quisquam adipisci odit officia, ratione porro recusandae temporibus cum dolor blanditiis distinctio veniam est voluptate, repellendus commodi, facere itaque velit.
+					</p>
+				</div>
+			</div>
+		</section>
+
+		<div class="case-study-quote">
+			<blockquote>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum quod iste debitis aliquam nihil delectus explicabo quibusdam enim deleniti autem, tempore, quis sint mollitia laudantium porro atque unde, saepe laborum.
+				</p>
+			</blockquote>
+		</div>
+
+		<section>
+			<div class="copy match">
+				<div class="inner">
+					<h2>Filming</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptas quisquam adipisci odit officia, ratione porro recusandae temporibus cum dolor blanditiis distinctio veniam est voluptate, repellendus commodi, facere itaque velit.
+					</p>
+				</div>
+			</div>
+			<div class="img match" style="background-image: url(/assets/img/case-studies/asda/3.png);"></div>
+		</section>
+
+		<section>
+			<div class="img match" style="background-image: url(/assets/img/case-studies/asda/4.png);"></div>
+			<div class="copy match">
+				<div class="inner">
+					<h2>Launch</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente voluptas quisquam adipisci odit officia, ratione porro recusandae temporibus cum dolor blanditiis distinctio veniam est voluptate, repellendus commodi, facere itaque velit.
+					</p>
+				</div>
+			</div>
+		</section>
+
+		<div class="finished-work">
+			<iframe src="http://www.youtube.com/embed/UMbNRTGwDJo?vq=hd1080&modestbranding=1&autoplay=0&controls=1&fs=0&iv_load_policy=3&rel=0&showinfo=0&autohide=0&disablekb=1" frameborder="0"></iframe>
+		</div>
+
+	</div>
+
+<?php include "partials/_footer.php" ?>
