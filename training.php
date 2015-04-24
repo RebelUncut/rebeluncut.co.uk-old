cf5742d493368b272a89ebd468f1bfe8411a5c23
Kevin Lewis <kevin@jellybabi.es>
2015-04-24 11:11:04 +0100

    Added /training

---
+++ training.php
@@ -0,0 +1,198 @@
+<?php 
+	$title = "Training";
+	$keywords = "key1";
+	include "partials/_header.php";
+?>
+
+	<div class="training">
+
+		<div class="page-title">
+			<div class="wrapper">
+				<h1>Training</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, nisi, nesciunt earum harum vitae nulla id perferendis culpa ab. 
+				</p>
+			</div>
+		</div>
+
+		<ul class="courses">
+			<li id="dslr-basics">
+				<h2>DSLR for Video Basics</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius beatae labore officiis iure nesciunt reiciendis dolor harum, possimus nulla.
+				</p>
+				<ul class="topics">
+					<h3>What you'll learn</h3>
+					<div class="content">
+						<li>Frame Rate</li>
+						<li>Aperture</li>
+						<li>ISO</li>
+						<li>White Balance</li>
+						<li>3 Point Lighting</li>
+						<li>Sound Setups</li>
+						<li>Tips &amp; Tricks</li>
+					</div>
+				</ul>
+				<ul class="faq">
+					<h3>FAQs</h3>
+					<div class="content">
+						<li>
+							<p>
+								This training is suitable for those who have a basic knowledge of photography or video production.
+							</p>
+						</li>
+						<li>
+							<p>
+								We'll provide a Canon 5D. 
+							</p>
+						</li>
+						<li>
+							<p>
+								Printed course materials are provided.
+							</p>
+						</li>
+					</div>
+				</ul>
+			</li>
+			<li id="dslr-advanced">
+				<h2>DSLR for Video Advanced Techniques</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius beatae labore officiis iure nesciunt reiciendis dolor harum, possimus nulla.
+				</p>
+				<ul class="topics">
+					<h3>What you'll learn</h3>
+					<div class="content">
+						<li>Combinining Filters for Effect</li>
+						<li>Advanced Lighting Techniques</li>
+						<li>DIY Lighting Solutions for On-Set Practials</li>
+						<li>Assorted Camera Rig Techniques</li>
+					</div>
+				</ul>
+				<ul class="faq">
+					<h3>FAQs</h3>
+					<div class="content">
+						<li>
+							<p>
+								This training is suitable for those who have a working knowledge of photography or video production.
+							</p>
+						</li>
+						<li>
+							<p>
+								We'll provide a Canon 5D.
+							</p>
+						</li>
+						<li>
+							<p>
+								Printed course materials are provided.
+							</p>
+						</li>
+					</div>
+				</ul>
+			</li>
+			<li id="edit-file">
+				<h2>Edit Training for File-Based Cameras</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius beatae labore officiis iure nesciunt reiciendis dolor harum, possimus nulla.
+				</p>
+				<ul class="topics">
+					<h3>What you'll learn</h3>
+					<div class="content">
+						<li>File Management Solutions for Different Filetypes</li>
+						<li>Ingestion in FCP, Premiere &amp; Avid</li>
+						<li>Sequence Settings for Optimum Resolutions</li>
+						<li>External Audio Syncing and Clip Grouping</li>
+					</div>
+				</ul>
+				<ul class="faq">
+					<h3>FAQs</h3>
+					<div class="content">
+						<li>
+							<p>
+								This training is suitable for experienced editors transitioning to file-based camera workflows.
+							</p>
+						</li>
+						<li>
+							<p>
+								We'll provide cards from various cameras, and an edit suite.
+							</p>
+						</li>
+						<li>
+							<p>
+								Printed course materials are provided.
+							</p>
+						</li>
+					</div>
+				</ul>
+			</li>
+			<li id="producing">
+				<h2>Being a Producer Basics</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius beatae labore officiis iure nesciunt reiciendis dolor harum, possimus nulla.
+				</p>
+				<ul class="topics">
+					<h3>What you'll learn</h3>
+					<div class="content">
+						<li>How to Budget Your Film</li>
+						<li>Effective Scheduling &amp; Communication</li>
+						<li>Managing a Production</li>
+						<li>Financing</li>
+					</div>
+				</ul>
+				<ul class="faq">
+					<h3>FAQs</h3>
+					<div class="content">
+						<li>
+							<p>
+								This training is suitable for 
+							</p>
+						</li>
+						<li>
+							<p>
+								We'll provide 
+							</p>
+						</li>
+						<li>
+							<p>
+								Printed course materials are provided.
+							</p>
+						</li>
+					</div>
+				</ul>
+			</li>
+			<li id="directing">
+				<h2>Directing Actors for Camera</h2>
+				<p>
+					During this day, you will work with a well-known director (eg Michael Baig-Clifford) to gain their insights and tips, allowing you to better engage with your actors. This will enable you to get the best performance possible out of your cast.
+				</p>
+			</li>
+			<li class="corp" id="film-day">
+				<h2>Film in a Day Corporate Experience</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius beatae labore officiis iure nesciunt reiciendis dolor harum, possimus nulla.
+				</p>
+				<ul class="topics">
+					<h3>Go through the steps</h3>
+					<div class="content">
+						<li>Storyboard</li>
+						<li>Planning</li>
+						<li>Filming</li>
+						<li>Editing</li>
+						<li>Sharing</li>
+					</div>
+				</ul>
+				<ul class="testimonial">
+					<div class="content">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium officiis tempore minima aliquid, dolores beatae maiores, eum cumque necessitatibus.
+						</p>
+						<cite>Clive Reeves, CliveReevesPR</cite>
+					</div>
+				</ul>
+			</li>
+		</ul>
+
+	</div>
+
+		
+
+<?php include "partials/_footer.php" ?>
