da465f8e51d509eed805810dd3c00a286da5a38c
NatHigginbottom <nat@rebeluncut.co.uk>
2015-05-08 22:09:36 +0100

    team pictures and egos

--- team.php
+++ team.php
@@ -15,80 +15,108 @@
 <ul class="team">
 	<li>
 		<img class="headshot" src="/assets/img/team/julia.jpg">
-		<img class="hero" src="/assets/img/team/carl.jpg">
+		<img class="hero" src="/assets/img/team/ironman.jpg">
 		<h2>Julia Higginbottom</h2>
 		<h3 class="role">Producer</h3>
-		<h3 class="hero">Most Like Iron Man</h3>
+		<h3 class="hero">Alter Ego -  Iron Man</h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/nat.png">
+		<img class="headshot" src="/assets/img/team/nat.png">
+		<img class="hero" src="/assets/img/team/thor.jpg">
 		<h2>Nat Higginbottom</h2>
-		<h3>Editor</h3>
+		<h3 class="role">Editor</h3>
+		<h3 class="hero">Alter Ego - Thor</h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/kevin.jpg">
+		<img class="headshot" src="/assets/img/team/kevin.jpg">
+		<img class="hero" src="/assets/img/team/war.jpg">
 		<h2>Kevin Lewis</h2>
-		<h3>Developer</h3>
+		<h3 class="role">Developer</h3>
+		<h3 class="hero">Alter Ego - War Machine</h3>
 	</li>
-	<li>
-		<img src="/assets/img/team/andrew.jpg">
+	<li>		
+		<img class="headshot" src="/assets/img/team/andrew.jpg">
+		<img class="hero" src="/assets/img/team/dead.png">
 		<h2>Andrew Sanyaolu</h2>
-		<h3>Developer</h3>
+		<h3 class="role">Developer</h3>
+		<h3 class="hero">Alter Ego - Deadpool</h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/rob.jpg">
+		<img class="headshot" src="/assets/img/team/rob.jpg">
+		<img class="hero" src="/assets/img/team/puck.jpg">
 		<h2>Rob Brown</h2>
-		<h3>Director</h3>
+		<h3 class="role">Director</h3>
+		<h3 class="hero">Alter Ego -  </h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/sima.jpg">
+		<img class="headshot" src="/assets/img/team/sima.jpg">
+		<img class="hero" src="/assets/img/team/hit.jpg">
 		<h2>Sima Gonsai</h2>
-		<h3>Director</h3>
+		<h3 class="role">Director</h3>
+		<h3 class="hero">Alter Ego - Hit Girl</h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/clare.jpg">
+		<img class="headshot" src="/assets/img/team/clare.jpg">
+		<img class="hero" src="/assets/img/team/anneka.jpg">
 		<h2>Clare Edwards</h2>
-		<h3>Producer</h3>
+		<h3 class="role">Producer</h3>
+		<h3 class="hero">Alter Ego - Anneka Rice</h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/carl.jpg">
+		<img class="headshot" src="/assets/img/team/carl.jpg">
+		<img class="hero" src="/assets/img/team/puck.jpg">
 		<h2>Carl Jordan</h2>
-		<h3>Cameraman</h3>
+		<h3 class="role">Cameraman</h3>
+		<h3 class="hero">Alter Ego -  </h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/sam.jpg">
+		<img class="headshot" src="/assets/img/team/sam.jpg">
+		<img class="hero" src="/assets/img/team/puck.jpg">
 		<h2>Sam Freeman</h2>
-		<h3>Visual Effects</h3>
+		<h3 class="role">Visual Effects</h3>
+		<h3 class="hero">Alter Ego -  </h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/steve.jpg">
+		<img class="headshot" src="/assets/img/team/steve.jpg">
+		<img class="hero" src="/assets/img/team/puck.jpg">
 		<h2>Stephen Atterbury</h2>
-		<h3>Visual Effects</h3>
+		<h3 class="role">Visual Effects</h3>
+		<h3 class="hero">Alter Ego -  </h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/phil.jpg">
+		<img class="headshot" src="/assets/img/team/phil.jpg">
+		<img class="hero" src="/assets/img/team/hans.jpg">
 		<h2>Phil Mountford</h2>
-		<h3>Composer</h3>
+		<h3 class="role">Composer</h3>
+		<h3 class="hero">Alter Ego - Hans Zimmer</h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/danny.jpg">
+		<img class="headshot" src="/assets/img/team/danny.jpg">
+		<img class="hero" src="/assets/img/team/puck.jpg">
 		<h2>Danny Smith</h2>
-		<h3>Writer</h3>
+		<h3 class="role">Writer</h3>
+		<h3 class="hero">Alter Ego -  </h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/billy.png">
+		<img class="headshot" src="/assets/img/team/billy.png">
+		<img class="hero" src="/assets/img/team/puck.jpg">
 		<h2>Billy Bannister</h2>
-		<h3>Location Sound</h3>
+		<h3 class="role">Location Sound</h3>
+		<h3 class="hero">Alter Ego -  </h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/caroline.jpg">
+		<img class="headshot" src="/assets/img/team/caroline.jpg">
+		<img class="hero" src="/assets/img/team/puck.jpg">
 		<h2>Caroline Beavon Ltd</h2>
-		<h3>Information/Data design</h3>
+		<h3 class="role">Information/Data design</h3>
+		<h3 class="hero">Alter Ego -  </h3>
 	</li>
 	<li>
-		<img src="/assets/img/team/dick.jpg">
+		<img class="headshot" src="/assets/img/team/dick.jpg">
+		<img class="hero" src="/assets/img/team/namor.jpg">
 		<h2>"Dick"</h2>
-		<h3>Office Mascot</h3>
+		<h3 class="role">Office Mascot</h3>
+		<h3 class="hero">Alter Ego - Namor</h3>
 	</li>
 </ul>
 
