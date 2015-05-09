5b55e5446d1f2b022273ab23c0c399a4151e6f5e
Kevin Lewis <kevin@jellybabi.es>
2015-05-09 16:16:48 +0100

    Optimised images, added XML sitemap, added humans.txt and a custom 404 page

--- partials/_header.php
+++ partials/_header.php
@@ -14,7 +14,7 @@
 	<meta property="og:site_name" content="Rebel Uncut"/>
 	<meta property="og:image:width" content="1200">
 	<meta property="og:image:height" content="630">
-	<meta property="og:image:ty pe" content="image/jpeg">
+	<meta property="og:image:type" content="image/jpeg">
 
     <meta name="description" content="155 Chars">
     <meta name="keywords" content="video birmingham,production birmingham,video production birmingham,corporate video birmingham,film production birmingham,film production london film production UK,post production birmingham,production company birmingham,TV production,advertising,web video,Live video,streaming,video streaming,web design,digital training,film production west midlands,video production west midlands,events,professional,promotional videos,studio,production,Conference Filming,Event Video,Event Filming,Seminar Filming,Cinema Adverts,Product Demonstration Video,Promo Video,Animation,Motion Graphics,<?php echo $keywords ?>">
